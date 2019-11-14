d8cf40f3f6b3185d8400a6c4cef6348d70e6fb76
Ryan Carins <ryan.carins@riscy.biz>
2019-11-15 00:49:01 +1100

    Add missing dollar sign from cost column

--- event_table.php
+++ event_table.php
@@ -15,7 +15,7 @@ if (mysqli_num_rows($table) > 0) {
 		echo "<tr>";
 			echo "<td>" . $row["name"]. "</td><td>" . $row["event_datetime"]. "</td><td>" .
 			$row["description"] . "</td><td>" . $row["location"] . "</td><td>" . $may_change 
-			. "</td><td>" . $row["cost"] . "</td>";
+			. "</td><td>$" . $row["cost"] . "</td>";
 		echo "</tr>";
 	}
 }
