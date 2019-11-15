d38edfd5f3f2fab73106c2b2cb37faa7de47eff0
Ryan Carins <ryan.carins@riscy.biz>
2019-11-15 21:39:00 +1100

    Filter out events not in the next fortnight

--- event_table.php
+++ event_table.php
@@ -6,17 +6,20 @@ $table= mysqli_query($mysqli, $event_query);
 
 if (mysqli_num_rows($table) > 0) {
 	while($row = mysqli_fetch_assoc($table)) {
-		if($row["may_change"] === 1){
-			$may_change = "Yes";
-		}else{
-			$may_change = "No";
-		}
+		if(time()<strtotime($row["event_datetime"]) &&
+			strtotime($row["event_datetime"]) < strtotime("+2 week")){
+			if($row["may_change"] === 1){
+				$may_change = "Yes";
+			}else{
+				$may_change = "No";
+			}
 
-		echo "<tr>";
-			echo "<td>" . $row["name"]. "</td><td>" . $row["event_datetime"]. "</td><td>" .
-			$row["description"] . "</td><td>" . $row["location"] . "</td><td>" . $may_change 
-			. "</td><td>$" . $row["cost"] . "</td>";
-		echo "</tr>";
+			echo "<tr>";
+				echo "<td>" . $row["name"]. "</td><td>" . $row["event_datetime"]. "</td><td>" .
+				$row["description"] . "</td><td>" . $row["location"] . "</td><td>" . $may_change 
+				. "</td><td>$" . $row["cost"] . "</td>";
+			echo "</tr>";
+		}
 	}
 }
 ?>
