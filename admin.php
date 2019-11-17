8234b6ab86739ba680eea8505666317bcf6fc5f2
Ryan Carins <ryan.carins@riscy.biz>
2019-11-17 17:57:32 +1100

    Style admin page and add it to navbar

--- admin.php
+++ admin.php
@@ -1,8 +1,18 @@
 <?php
 	require("check_login.php");
 ?>
-<html>
-<body>
-<h1>Admin Page</h1>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes"/>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <?php
+      include('navbar.php');
+	?>
+	<h2>Admin Page</h2>
+  </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -3,4 +3,5 @@
   <li><a href="events.php">Events</a></li>
   <li><a href="contact.php">Contacts</a></li>
   <li><a href="member_signup.php">Signup</a></li>
+  <li><a href="login_page.php">Admin Login</a></li>
 </ul>
