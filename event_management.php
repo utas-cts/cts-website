53f826dc8ea547624c79219ee2e21f61d165a001
Ryan Carins <ryan.carins@riscy.biz>
2019-11-17 22:09:21 +1100

    Add basic event management page

---
+++ event_management.php
@@ -0,0 +1,18 @@
+<?php
+	require("check_login.php");
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Event Management</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes"/>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <?php
+      include('navbar.php');
+	?>
+	<h2>Event management</h2>
+  </body>
+</html>
