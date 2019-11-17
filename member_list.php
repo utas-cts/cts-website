9b16468a448d01d9d970b9bceb7b158842baf3f4
Ryan Carins <ryan.carins@riscy.biz>
2019-11-18 03:26:51 +1100

    Change member list page title to be appropriate

--- member_list.php
+++ member_list.php
@@ -5,7 +5,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Admin</title>
+    <title>Member List</title>
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes"/>
     <link rel="stylesheet" href="css/style.css">
   </head>
@@ -32,7 +32,10 @@
       $query="SELECT * FROM members";
 	  $result=mysqli_query($mysqli, $query);
 	  while($row = mysqli_fetch_assoc($result)){
-		  echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["last_name"] . "</td><td>" . $row["member_type"] . "</td><td>" . $row["email"] . "</td><td>" . $row["signup_date"] . "</td><td>" . $row["payment_date"] . "</td><td>" . $row["payment_status"] . "</td><td>" . $row["photo_allowed"] . "</td><td>" . $row["email_allowed"] ."</td></tr>";
+		  echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["last_name"] .
+			  "</td><td>" . $row["member_type"] . "</td><td>" . $row["email"] . "</td><td>" . $row["signup_date"] .
+			  "</td><td>" . $row["payment_date"] . "</td><td>" . $row["payment_status"] .
+			  "</td><td>" . $row["photo_allowed"] . "</td><td>" . $row["email_allowed"] ."</td></tr>";
 	  }
 	?>
 	</table>
