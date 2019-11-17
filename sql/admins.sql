0144ee555218e725aa571946d4f03f8c3ae529d4
Ryan Carins <ryan.carins@riscy.biz>
2019-11-17 17:23:47 +1100

    Change admin primary key to username This is because we don't need an id and there shouldn't be duplicate usernames

--- sql/admins.sql
+++ sql/admins.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS admins(
-	ID int(11) NOT NULL AUTO_INCREMENT,
 	username varchar(32) NOT NULL,
 	hashed_password varchar(256) NOT NULL,
-	primary key(ID)
+	primary key(username)
 );
