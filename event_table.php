c6416a60705d94c99f2713ce2be0ea6289192cd1
Ryan Carins <ryan.carins@riscy.biz>
2019-11-19 09:13:06 +1100

    Display event Id if the user has admin rights

--- event_table.php
+++ event_table.php
@@ -2,19 +2,26 @@
 include("db_connect.php");
 $date = date("Y-m-d H:i:s");
 $fortnight = date("Y-m-d H:i:s", strtotime("+2 week"));
-$event_query="SELECT name, event_datetime, description, location, may_change, cost from events where event_datetime > '$date' and event_datetime < '$fortnight'";
+if($_SESSION["authorised"] == true){
+	$event_query="SELECT id, name, event_datetime, description, location, may_change, cost from events where event_datetime > '$date' and event_datetime < '$fortnight'";
+}else{
+	$event_query="SELECT name, event_datetime, description, location, may_change, cost from events where event_datetime > '$date' and event_datetime < '$fortnight'";
+}
 $table= mysqli_query($mysqli, $event_query);
 
 if (mysqli_num_rows($table) > 0) {
-echo "<table>
-	    <tr>
-		  <th>Event</th>
-		  <th>Date</th>
-		  <th>Description</th>
-		  <th>Location</th>
-		  <th>May change</th>
-	  	  <th>Cost</th>
-		</tr>";
+		echo "<table>
+				<tr>";
+		if($_SESSION["authorised"] == true){
+				  echo "<th>Id</th>";
+		}
+			 echo "<th>Event</th>
+				  <th>Date</th>
+				  <th>Description</th>
+				  <th>Location</th>
+				  <th>May change</th>
+			 	  <th>Cost</th>
+				</tr>";
 	while($row = mysqli_fetch_assoc($table)) {
 		if($row["may_change"] == 1){
 			$may_change = "Yes";
@@ -23,6 +30,9 @@ echo "<table>
 		}
 
 		echo "<tr>";
+		if($_SESSION["authorised"] == true){
+			echo "<td>" . $row["id"] . "</td>";
+		}
 			echo "<td>" . $row["name"]. "</td><td>" . date_format(date_create($row["event_datetime"]),"D M dS ga") . "</td><td>" .
 				$row["description"] . "</td><td>" . $row["location"] . "</td><td>" . $may_change 
 				. "</td><td>$" . $row["cost"] . "</td>";
