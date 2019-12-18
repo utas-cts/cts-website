2b1be558c92f185507da5148ac7b593bde7e4ffa
Ryan Carins <ryan.carins@riscy.biz>
2019-12-19 04:07:28 +1100

    Remove some more redundant stuff

--- admin/add_admin.php
+++ admin/add_admin.php
@@ -1,6 +1,6 @@
 <?php
-    require '/include/check_login.php';
-    require '/include/db_connect.php';
+    require 'check_login.php';
+    require 'db_connect.php';
     $query = $mysqli->prepare('INSERT INTO admins (username, hashed_password) values (?, ?)');
     $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $query->bind_param('ss', $_POST['username'], $hash);
--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-require('db_connect.php');
+require 'db_connect.php';
 
 if ($_POST['photograph_allowed'] === 'on') {
     $photo_allowed = 1;
