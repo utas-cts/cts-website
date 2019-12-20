c94ba7393c83daf41fc24aab9789414aace5b15e
Ryan Carins <ryan.carins@riscy.biz>
2019-12-21 00:52:46 +1100

    Scale project management for mobile

--- admin/project_management.php
+++ admin/project_management.php
@@ -8,6 +8,7 @@
     <title>Project Management</title>
     <meta name='viewport' content='width=device-width,initial-scale=1,user-scalable=yes'/>
     <link rel='stylesheet' href='/css/style.css'>
+	<link rel='stylesheet' href='/css/mobile.css'>
   </head>
   <body>
     <?php
