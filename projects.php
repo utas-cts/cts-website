89aeae6a0cde7608d87557db07a639a7d7a65100
Ryan Carins <ryan.carins@riscy.biz>
2019-12-20 18:00:47 +1100

    Setup project table for mobile scaling

--- projects.php
+++ projects.php
@@ -6,6 +6,7 @@
     <title>CTS Projects</title>
     <meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
     <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/mobile.css">
   </head>
   <body>
     <?php
