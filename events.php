766ec11f48b435a7afb271a828aea8b14f87b8b4
Ryan Carins <ryan.carins@riscy.biz>
2019-12-20 17:29:27 +1100

    Change the way tables are displayed on mobile

--- events.php
+++ events.php
@@ -6,15 +6,18 @@
     <title>CTS Events</title>
     <meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
     <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/mobile.css">
   </head>
   <body>
     <?php
       include('navbar.php');
     ?>
-    <h2>Todays Date: <?php echo date('M dS')?></h2>
+	<h2>Todays Date: <?php echo date('M dS')?></h2>
+	<div style="overflow-x:auto;">
     <?php
           include('event_table.php');
-    ?>
+	?>
+	</div>
     <p>Please contact us if you wish to suggest an event at the <a href="contact.php">contacts page</a></p>
   </body>
 </html>
