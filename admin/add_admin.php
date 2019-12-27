a063159dcefcf53a742a929c9b1837dbc65a4373
Ryan Carins <ryan.carins@riscy.biz>
2019-12-27 22:03:17 +1100

    Migrate to argon2 This is more secure than bcrypt and allows for passwords above 72 characters in length

--- admin/add_admin.php
+++ admin/add_admin.php
@@ -4,7 +4,7 @@ require 'db_connect.php';
 $query = $mysqli->prepare(
     'INSERT INTO admins (username, hashed_password) values (?, ?)'
 );
-$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$hash = password_hash($_POST['password'], PASSWORD_ARGON2I);
 $query->bind_param('ss', $_POST['username'], $hash);
 $query->execute();
 header('Location: ./manage_admins.php');
