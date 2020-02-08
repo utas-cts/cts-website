bda961518c634d6049647d8b256f7d896f7c1db8
Ryan Carins <ryan.carins@riscy.biz>
2020-02-09 03:53:26 +1100

    Create tables

---
+++ sql/memberships.sql
@@ -0,0 +1,13 @@
+CREATE TABLE memberships(
+	ID INT(11) AUTO_INCREMENT NOT NULL,
+	member INT(11) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	signup_date DATE NOT NULL,
+	payment_date DATE,
+	semester_1 BOOLEAN NOT NULL DEFAULT 0,
+	semester_2 BOOLEAN NOT NULL DEFAULT 0,
+	photo_allowed BOOLEAN NOT NULL,
+	email_allowed BOOLEAN NOT NULL,
+	primary key (ID),
+	foreign key (member) references members(ID)
+);
