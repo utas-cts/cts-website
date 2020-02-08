0c3ac136c11defede064aaaa32dde1f1a7ceb4a0
Ryan Carins <ryan.carins@riscy.biz>
2020-02-09 03:35:09 +1100

    Add partially complete contacts form

--- contact.php
+++ contact.php
@@ -14,6 +14,28 @@
        require 'navbar.php';
     ?>
     <div id="content-wrap">
+    <div class="row">
+    <div class="column">
+      <h2>Suggestions</h2>
+      <form action='./add_suggestion.php' method='post'>
+      <label for="suggestion">Suggestion (max 1000 characters)</label>
+      <br>
+      <textarea id="suggestion" name="suggestion" maxLength="1000" required></textarea>
+      <br>
+      <button>Submit</button>
+      </form>
+    </div>
+    <div class="column">
+    <h2>General Inquiries</h2>
+    <p>
+    For general inquiries you can email us at cts.utas@gmail.com or fill in the form below
+    we endeavour to answer all correspondence promptly, however in some instances it may take
+    a day or two for a reply
+    </p>
+    <form>
+    </form>
+    </div>
+    </div>
     </div>
     <?php
       require 'footer.php';
--- index.php
+++ index.php
@@ -18,15 +18,26 @@
     <h2>UTAS Computing & Technology Society (CTS)</h2>
     <p>
     The CTS is a TUU affiliated society providing value
-    to Launceston attending students of UTAS. Now in our fifth year, our society has won
-    awards including best society three years in a row and remain committed to continuing
-    to offer events for students of any background. Get in contact with us or
-    <a href='member_signup.php'>signup</a> today
-    to learn more.
+    to Launceston attending students of UTAS. Established in 2016 and now in our fifth year,
+    our society has won awards including best society three years in a row and remain
+    committed to continuing to offer events for students of any background. Get in contact
+    with us or <a href='member_signup.php'>signup</a> today to learn more.
     </p>
     </div>
     <div id="events">
-    <?php
+    <h2>Events</h2>
+    <h3>What we offer</h3>
+    <p>
+    The CTS try to run at least one event a week, we frequently run barbecues where members eat
+    free, we have games days, bowling nights, and D&D days to name a few. While we are the Computing 
+    and Technology society, only some of our events relate to technology, most are simply
+    opportunities to socialise with other students/staff. We run events both on and off the UTAS Newnham
+    campus so that students have a greater chance of being able to attend. If you have any ideas for events
+    that you would like to attend please post a suggestion at the <a href='contact.php'>contacts page.</a>
+    </p>
+
+    <h3>Todays Event/s</h3>
+        <?php
         require 'event_table.php';
         if(!print_events(1)){
             echo 'There are no events today. Visit the <a href="events.php">events page</a>
