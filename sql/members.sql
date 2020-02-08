7575a4a51ab29d974e5548792e680e4f9a0559cc
Ryan Carins <ryan.carins@riscy.biz>
2020-02-09 05:36:28 +1100

    Make student ID unique

--- sql/members.sql
+++ sql/members.sql
@@ -1,6 +1,6 @@
 CREATE TABLE members(
 	ID INT(11) AUTO_INCREMENT NOT NULL,
-	student_id  INT(11),
+	student_id  INT(11) UNIQUE,
 	first_name VARCHAR(32) NOT NULL,
 	last_name VARCHAR(32) NOT NULL,
 	member_type ENUM('Student','Other') NOT NULL,
