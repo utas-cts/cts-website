731c93c1cbd4872ece10e9266c0b5f0ce9a99907
Ryan Carins <ryan.carins@riscy.biz>
2020-02-09 20:39:05 +1100

    Center content add margins

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,
     user-scalable=yes"/>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mobile.css">
   </head>
   <body>
     <div id="page-container">
